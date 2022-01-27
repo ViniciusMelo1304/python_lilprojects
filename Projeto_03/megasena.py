cd4be62257b4b9a7b721657bc201c4bb913f385a
ViniciusMelo1304 <vinicius.melo1304@gmail.com>
2022-01-27 15:10:39 -0300

    trim do código

--- Projeto_03/megasena.py
+++ Projeto_03/megasena.py
@@ -2,6 +2,9 @@
 Desafio - Verificar a baixa probabilidade de acertar um jogo na Mega Sena.
 '''
 
+import time
+
+
 def MegaSena():
     sorteio = []
     import random
@@ -9,15 +12,16 @@ def MegaSena():
         número = random.randint(1, 60)
         if número not in sorteio:
             sorteio.append(número)
-        else:
-            pass
         sorteio.sort()
     return sorteio
 
 
+tempo_inicial = time.time()  # em segundos
+
 Jogo = []
 numJogos = 0
-Jogo_Usuário = [1, 2, 3, 4, 5, 6]
+Jogo_Usuário = MegaSena()
+print(Jogo_Usuário)
 
 '''
 Caso o usuário queira digitar um jogo específico, descomentar esta parte e comentar a variável Jogo_Usuário acima.
@@ -32,4 +36,7 @@ while Jogo_Usuário != Jogo:
     Jogo = MegaSena()
     numJogos += 1
 
+tempo_final = time.time()  # em segundos
+
+print(f"O programa demorou {tempo_final - tempo_inicial:0f} segundos para encontrar um jogo, de forma randômica, que fosse equivalente ao do usuário.")
 print(numJogos)
