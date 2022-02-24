fadadd2b367ed4aa9dd25b3a528aa6f86b1df725
ViniciusMelo1304 <vinicius.melo1304@gmail.com>
2022-02-24 16:27:01 -0300

    Refatorando Projeto 02 (Probabilidade)

--- probabilidade_Dados.py
+++ probabilidade_Dados.py
@@ -8,36 +8,31 @@ def Dado():
     return dado
 
 
-contagem = 0
-resultado = []
-d1 = d2 = d3 = d4 = d5 = d6 = 0
+print()
 
-while contagem < 10000000:  # Define quantas vezes o dado será 'lançado'
-    resultado.append(Dado())  # Adiciona o resultado do dado à uma lista
-    contagem += 1
+# Define quantas vezes o dado será 'lançado'
+limite = int(input('Quantas vezes você quer lançar o dado?\n'))
+frequencias = [0 for i in range(6)]
+probabilidades = [0 for i in range(6)]
 
-for dado in range(len(resultado)):  # Calcula a soma dos resultados de cada face
-    if resultado[dado] == 1:
-        d1 += 1
-    elif resultado[dado] == 2:
-        d2 += 1
-    elif resultado[dado] == 3:
-        d3 += 1
-    elif resultado[dado] == 4:
-        d4 += 1
-    elif resultado[dado] == 5:
-        d5 += 1
-    else:
-        d6 += 1
 
-prob1 = d1/len(resultado)  # Calcula as probabilidades
-prob2 = d2/len(resultado)
-prob3 = d3/len(resultado)
-prob4 = d4/len(resultado)
-prob5 = d5/len(resultado)
-prob6 = d6/len(resultado)
+# Lança o dados 'limite' vezes e armazena as frequências
+for lançamento in range(limite):
+    dado = Dado()  # 'Lança' o dado
+    for index, _ in enumerate(frequencias):
+        if dado-1 == index:
+            frequencias[index] += 1
+            continue
 
-print("1: {} vezes.\n2: {} vezes.\n3: {} vezes.\n4: {} vezes.\n5: {} vezes.\n6: {} vezes.\n".format(
-    d1, d2, d3, d4, d5, d6))  # Mostra quantas vezes cada dado foi lançado
-print("Prob1: {}.\nProb2: {}.\nProb3: {}.\nProb4: {}.\nProb5: {}.\nProb6: {}.\n".format(
-    prob1, prob2, prob3, prob4, prob5, prob6))  # Mostra a probabilidade de cada face do dado
+print()
+
+# Calcula as probabilidades e imprime as frequências
+for indice, frequencia in enumerate(frequencias):
+    probabilidades[indice] = frequencia/limite
+    print(f'Face {indice + 1}:\t{frequencia:02}x')
+
+print()
+
+# Imprime as probabilidades
+for indice, probabilidade in enumerate(probabilidades):
+    print(f'Prob {indice + 1}:\t{probabilidade:2.2%}')
